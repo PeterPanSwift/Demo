26f0accabd2ba9ee01b00b64338454c21f45cc6f
peter pan <apppeterpan@gmail.com>
2025-02-21 12:54:30 +0800

    Initial Commit

---
+++ Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by SHIH-YING PAN on 2025/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
